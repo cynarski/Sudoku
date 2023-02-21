82baf78fa188e92da49d4b5f7dea9c21e4c06953
Michał <cynarski60@gmail.com>
2023-02-21 18:26:08 +0100

    levels done

--- start.py
+++ start.py
@@ -5,36 +5,34 @@ from kivy import Config
 import random
 import proby
 
-
 Builder.load_file('startkivy.kv', rulesonly=True)
+
 
 class Start(FloatLayout):
 
-
     def easy_button_click(self):
-        counter = random.randint(1,2)
+        counter = random.randint(10, 20)
         print(counter)
         App.get_running_app().stop()
-        proby.SudokuApp(counter).run()
-
+        level = "EASY"
+        proby.SudokuApp(counter, level).run()
 
     def medium_button_click(self):
         counter = random.randint(21, 35)
         print(counter)
         App.get_running_app().stop()
-        proby.SudokuApp(counter).run()
+        level = "MEDIUM"
+        proby.SudokuApp(counter, level).run()
 
     def hard_button_click(self):
         counter = random.randint(35, 55)
         print(counter)
         App.get_running_app().stop()
-        proby.SudokuApp(counter).run()
+        level = "HARD"
+        proby.SudokuApp(counter, level).run()
 
 
 class StartApp(App):
 
     def build(self):
         return Start()
-
-
-
